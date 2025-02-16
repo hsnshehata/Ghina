a3c6950f2f80202d790fe468f9f75a11b2d0378b
hsnshehata <hsn,shehata@gmail.com>
2025-02-16 11:46:12 +0200

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false, // يمكنك تغييره إلى true إذا أردت
+  experimental: {
+    appDir: true,
+  },
+};
+
+module.exports = nextConfig;
